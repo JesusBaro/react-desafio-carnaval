30762728a4888b60e6a9bc3a664b1df78e886d76
Jésus Baro <118184872+JesusBaro@users.noreply.github.com>
2023-02-17 13:19:41 -0300

    erro no react e organizando pastas

--- desafio-carnaval/src/App.js
+++ desafio-carnaval/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+//cada parte é um componente 
+import "./files/style.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+import React, {Component} from 'react'
+
+  class header extends Component{
+
+    render(){
+      return(
+        <section class="header">
+<h1>oi</h1>
+
+        </section>
+      )
+    }
+  }
+  
+export default header
