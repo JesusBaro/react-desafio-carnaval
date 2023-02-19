f120d35cb92e2d744630b696b5b69ee82ca3e4d7
Jésus Baro <118184872+JesusBaro@users.noreply.github.com>
2023-02-19 15:38:13 -0300

    atualizando projeto

--- desafio-carnaval/src/App.js
+++ desafio-carnaval/src/App.js
@@ -1,18 +1,50 @@
 //cada parte é um componente
+import bruxa from"./img/bruxa.png"
+import fada from "./img/fada.png"
+import"./files/style.css"
+
 
 
 import React, {Component} from 'react'
 
-  class header extends Component{
+  class CarnavalPage extends Component{
 
     render(){
-      return(
-        <section class="header">
-<h1>oi</h1>
+return(
+ 
+  
+<body>
 
+        <div class="header">
+        <h1>Carnaval Vai na Web</h1>
+        </div>,
+
+        <section class="Content">
+        <div class="CostumeBox">`${bruxa}`</div>
+        <div class="CostumeBox">${fada}`</div>
+        <div class="CostumeBox">img</div>
         </section>
+        <section class="Content">
+        <div class="CostumeBox">img</div>
+        <div class="CostumeBox">img</div>
+        <div class="CostumeBox">img</div>
+        </section>,
+
+
+
+        <footer class="footer">
+        <div class="CostumeBoxFooter"></div>
+        <div class="CostumeBoxFooter"></div>
+        <div class="CostumeBoxFooter"></div>
+        <div class="CostumeBoxFooter"></div>
+        <div class="CostumeBoxFooter"></div>
+        <div class="CostumeBoxFooter"></div>
+        <div class="CostumeBoxFooter"></div>
+       </footer>
+</body>
+  
       )
     }
   }
   
-export default header
+export default CarnavalPage
